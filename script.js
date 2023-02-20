329dfa4a554dea2c1f09908db0f11476dc426dce
Olga Parfenova <work.parfenova@gmail.com>
2023-02-20 20:05:28 +0300

    The second module is completed

---
+++ script.js
@@ -0,0 +1,112 @@
+// task 1
+
+// let obj = {
+//     1: 'Monday',
+//     2: 'Tuesday',
+//     3: 'Wednesday',
+//     4: 'Thursday',
+//     5: 'Friday',
+//     6: 'Saturday',
+//     7: 'Sunday',
+// };
+//
+// console.log(obj[1]);
+// console.log(obj[2]);
+// console.log(obj[3]);
+// console.log(obj[4]);
+// console.log(obj[5]);
+// console.log(obj[6]);
+// console.log(obj[7]);
+
+// task 2
+
+// let obj = {
+//     1: 'Jan',
+//     2: 'Feb',
+//     3: 'Mar',
+//     4: 'Apr',
+//     5: 'May',
+//     6: 'Jun',
+//     7: 'Jul',
+//     8: 'Aug',
+//     9: 'Sep',
+//     10: 'Oct',
+//     11: 'Nov',
+//     12: 'Dec',
+// }
+//
+// console.log(obj);
+
+// task 3
+
+// let user = {
+//     name: 'Peter',
+//     surname: 'Parker',
+//     patronymic: 'Parkovich',
+// }
+//
+// const {name, surname, patronymic} = user;
+// console.log(`${name} ${surname} ${patronymic}`);
+
+// task 4
+
+// const date = {
+//     year: 2023,
+//     month: 'February',
+//     day: 20,
+// }
+//
+// const {year, month, day} = date;
+//
+// console.log(`${year}-${month}-${day}`);
+
+// task 5
+
+// let obj = {
+//     '1a': 1,
+//     'b2': 2,
+//     'c-c': 3,
+//     'd 4': 4,
+//     'e5': 5
+// };
+//
+// console.log(obj['c-c']);
+
+// task 6
+
+// let obj = {x: 1, y: 2, z: 3};
+//
+// console.log(obj.x * obj.x);
+// console.log(obj.y * obj.y);
+// console.log(obj.z * obj.z);
+
+// task 7
+
+// let obj = {};
+//
+// obj['1'] = 'd';
+// obj['2'] = 'fgegt';
+// obj['3'] = 18;
+// obj['4'] = true;
+//
+// console.log(obj);
+
+// task 8
+
+// let obj = {x: 1, y: 2, z: 3};
+// const keys = Object.keys(obj);
+// console.log(keys);
+
+// task 9
+
+// let obj = {x: 1, y: 2, z: 3};
+// const length = Object.keys(obj).length;
+// console.log(length);
+
+// task 10
+
+// let obj = {x: 1, y: 2, z: 3};
+// const key = 'y';
+// console.log(obj[key]);
+
+
