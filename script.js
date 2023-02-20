fbd6055e51a6f7087f6e87fd15def81b6280400f
Olga Parfenova <work.parfenova@gmail.com>
2023-02-20 20:50:05 +0300

    The fourth module is completed

--- script.js
+++ script.js
@@ -160,4 +160,24 @@
 
 // task 17
 
+// let obj = {x: 1, y: 2, z: 3};
+// console.log(obj['x']);
 
+// task 18
+
+// let obj = {x: 1, y: 2, z: 3};
+// let key = 'x';
+//
+// console.log(obj[key]);
+
+// task 19
+
+// let obj = {x: 1, y: 2, z: 3};
+// let sum = obj['x'] + obj['y'] + obj['z'];
+//
+// console.log(sum);
+
+// task 20
+
+// let obj = {x: 1, y: 2, z: 3};
+// console.log(Object.keys(obj).length);
