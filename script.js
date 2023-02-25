d98c008aad7b10a24c7a3858949fa9e725018139
Olga Parfenova <work.parfenova@gmail.com>
2023-02-25 23:53:59 +0300

    The sixth module is completed

--- script.js
+++ script.js
@@ -243,3 +243,159 @@
 // } else {
 //     console.log('---');
 // }
+
+// task 27
+
+// let obj = {
+//     key1: {
+//         key1: 1,
+//         key2: 2,
+//         key3: 3,
+//     },
+//     key2: {
+//         key1: 4,
+//         key2: 5,
+//         key3: 6,
+//     },
+//     key3: {
+//         key1: 7,
+//         key2: 8,
+//         key3: 9,
+//     },
+// }
+//
+// console.log(obj["key1"]["key1"] + obj["key1"]['key2'] + obj['key1']['key3'] + obj['key2']['key1'] + obj['key2']['key2']
+//     + obj['key2']['key3'] + obj['key3']['key1'] + obj['key3']['key2'] + obj['key3']['key3']);
+
+// task 28
+
+// let obj = {
+//     1: {
+//         1: 'a1',
+//         2: 'a2',
+//         3: 'a3',
+//     },
+//     2: {
+//         1: 'b1',
+//         2: 'b2',
+//         3: 'b3',
+//     },
+//     3: {
+//         1: 'c1',
+//         2: 'c2',
+//         3: 'c3',
+//     },
+// }
+//
+// console.log(obj['2']['2']);
+// console.log(obj['3']['1']);
+
+// task 29
+
+// let obj = {
+//     key1: {
+//         a: 1,
+//         b: 2,
+//         c: {
+//             d: 3,
+//             e: 4,
+//         },
+//         f: 5,
+//     },
+//     key2: {
+//         g: 6,
+//         h: 7,
+//     },
+// }
+//
+// console.log(obj['key1']['a'] + obj['key1']['b'] + obj['key1']['c']['d'] + obj['key1']['c']['e']
+//     + obj['key1']['f'] + obj['key2']['g'] + obj['key2']['h']);
+
+// task 30
+
+// let obj = {
+//     1: {
+//         1: 11,
+//         2: 12,
+//         3: 13,
+//     },
+//     2: {
+//         1: 21,
+//         2: 22,
+//         3: 23,
+//     },
+//     3: {
+//         1: 24,
+//         2: 25,
+//         3: 26,
+//     },
+// }
+//
+// let sum = 0;
+//
+// for (let key in obj) {
+//     let subObj = obj[key];
+//
+//     for (let subKey in subObj) {
+//         sum += subObj[subKey];
+//     }
+// }
+//
+// console.log(sum);
+
+// task 31
+
+// let obj = {
+//     1: {
+//         1: {
+//             1: 111,
+//             2: 112,
+//             3: 113,
+//         },
+//         2: {
+//             1: 121,
+//             2: 122,
+//             3: 123,
+//         },
+//     },
+//     2: {
+//         1: {
+//             1: 211,
+//             2: 212,
+//             3: 213,
+//         },
+//         2: {
+//             1: 221,
+//             2: 222,
+//             3: 223,
+//         },
+//     },
+//     3: {
+//         1: {
+//             1: 311,
+//             2: 312,
+//             3: 313,
+//         },
+//         2: {
+//             1: 321,
+//             2: 322,
+//             3: 323,
+//         },
+//     },
+// }
+//
+// let sum = 0;
+//
+// for (let key in obj) {
+//     let subObj = obj[key];
+//
+//     for (let subKey in subObj) {
+//         let subSubObj = subObj[subKey];
+//
+//         for (let subSubKey in subSubObj) {
+//             sum += subSubObj[subSubKey];
+//         }
+//     }
+// }
+//
+// console.log(sum);
