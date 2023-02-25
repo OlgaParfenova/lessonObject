c1d2c6992473ae1da90c38356adad274d8678087
Olga Parfenova <work.parfenova@gmail.com>
2023-02-26 01:58:24 +0300

    The seventh module is completed

--- script.js
+++ script.js
@@ -399,3 +399,415 @@
 // }
 //
 // console.log(sum);
+
+// task 32
+
+// let students = {
+//     'group1': ['name11', 'name12', 'name13'],
+//     'group2': ['name21', 'name22', 'name23'],
+//     'group3': ['name31', 'name32', 'name33'],
+// };
+//
+// console.log(students['group3'][0]);
+
+// task 33
+
+// let data = {
+//     1: [
+//         'data11',
+//         'data12',
+//         'data13',
+//     ],
+//     2: [
+//         'data21',
+//         'data22',
+//         'data23',
+//     ],
+//     3: [
+//         'data31',
+//         'data32',
+//         'data33',
+//     ],
+//     4: [
+//         'data41',
+//         'data42',
+//         'data43',
+//     ],
+// };
+//
+// for (let num in data) {
+//     for (let str of data[num]) {
+//         console.log(str);
+//     }
+// }
+
+// task 34
+
+// let data = [
+//     {
+//         1: 'data11',
+//         2: 'data12',
+//         3: 'data13',
+//     },
+//     {
+//         1: 'data21',
+//         2: 'data22',
+//         3: 'data33',
+//     },
+//     {
+//         1: 'data31',
+//         2: 'data32',
+//         3: 'data33',
+//     },
+// ];
+//
+// for (let obj of data) {
+//     for (let key in obj) {
+//         console.log(obj[key]);
+//     }
+// }
+
+// task 35
+
+// let data = [
+//     {
+//         1: [
+//             'data111',
+//             'data112',
+//             'data113',
+//         ],
+//         2: [
+//             'data121',
+//             'data122',
+//             'data123',
+//         ],
+//     },
+//     {
+//         1: [
+//             'data211',
+//             'data212',
+//             'data213',
+//         ],
+//         2: [
+//             'data221',
+//             'data222',
+//             'data223',
+//         ],
+//     },
+//     {
+//         1: [
+//             'data411',
+//             'data412',
+//             'data413',
+//         ],
+//         2: [
+//             'data421',
+//             'data422',
+//             'data423',
+//         ],
+//     },
+// ];
+//
+// for (let obj of data) {
+//     for (let key in obj) {
+//         for (let elem of obj[key]) {
+//             console.log(elem);
+//         }
+//     }
+// }
+
+// task 36
+
+// let employees = [
+//     {
+//         name: 'name1',
+//         salary: 300,
+//     },
+//     {
+//         name: 'name2',
+//         salary: 400,
+//     },
+//     {
+//         name: 'name3',
+//         salary: 500,
+//     },
+// ];
+//
+// for (let elem of employees) {
+//     console.log(elem.name + '-' + elem.salary);
+// }
+
+// task 37
+
+// let employees = [
+//     {
+//         name: 'name1',
+//         salary: 300,
+//     },
+//     {
+//         name: 'name2',
+//         salary: 400,
+//     },
+//     {
+//         name: 'name3',
+//         salary: 500,
+//     },
+// ];
+//
+// let sum = 0;
+//
+// for (let elem of employees) {
+//     sum += elem.salary;
+// }
+//
+// console.log(sum);
+
+// task 38
+
+// let employees = [
+//     {
+//         name: 'name1',
+//         salary: 300,
+//         age: 28,
+//     },
+//     {
+//         name: 'name2',
+//         salary: 400,
+//         age: 29,
+//     },
+//     {
+//         name: 'name3',
+//         salary: 500,
+//         age: 30,
+//     },
+//     {
+//         name: 'name4',
+//         salary: 600,
+//         age: 31,
+//     },
+//     {
+//         name: 'name5',
+//         salary: 700,
+//         age: 32,
+//     },
+// ];
+//
+// let sum = 0;
+//
+// for (let elem of employees) {
+//     if (elem.age >= 30) {
+//         sum += elem.salary;
+//     }
+// }
+//
+// console.log(sum);
+
+// task 39
+
+// let months = {
+//     'ru': [
+//         'январь',
+//         'февраль',
+//         'март',
+//         'апрель',
+//         'май',
+//         'июнь',
+//         'июль',
+//         'август',
+//         'сентябрь',
+//         'октябрь',
+//         'ноябрь',
+//         'декабрь',
+//     ],
+//     'en': [
+//         'january',
+//         'february',
+//         'march',
+//         'april',
+//         'may',
+//         'june',
+//         'july',
+//         'august',
+//         'september',
+//         'october',
+//         'november',
+//         'december',
+//     ],
+// };
+//
+// let lang = 'ru'; // может быть или 'ru' или 'en'
+// let month = 5;   // число от 0 до 11
+//
+// console.log(months[lang][month]);
+
+// task 40
+
+// let affairs = {
+//     '2018': {
+//         11: {
+//             29: ['дело111', 'дело112', 'дело113'],
+//             30: ['дело121', 'дело122', 'дело123'],
+//         },
+//         12: {
+//             30: ['дело211', 'дело212', 'дело213'],
+//             31: ['дело221', 'дело222', 'дело223'],
+//         },
+//     },
+//     '2019': {
+//         12: {
+//             29: ['дело311', 'дело312', 'дело313'],
+//             30: ['дело321', 'дело322', 'дело323'],
+//             31: ['дело331', 'дело332', 'дело333'],
+//         }
+//     },
+// }
+//
+// let year = 2019;
+// let month = 12;
+// let day = 30;
+//
+// console.log(affairs[year][month][day]);
+
+// task 41
+
+// let obj = {
+//     key1: {
+//         key2: '12',
+//         key3: '13',
+//     },
+//     key2: {
+//         key4: '24',
+//         key5: '25',
+//     },
+// }
+//
+// let key1 = 'key2';
+// let key2 = 'key4';
+//
+// console.log(obj[key1][key2]);
+
+// task 42
+
+// let obj = {
+//     key1: {
+//         key2: '12',
+//         key3: '13',
+//     },
+//     key2: {
+//         key4: '24',
+//         key5: '25',
+//     },
+// }
+//
+// let key1 = 'key2';
+// let key2 = 'key4';
+//
+// console.log(obj[key1][key2]);
+
+// task 43
+
+// let obj = {
+//     key1: {
+//         key2: '12',
+//         key3: '13',
+//     },
+//     key2: {
+//         key4: '24',
+//         key5: '25',
+//     },
+// }
+//
+// let key1 = 'key2';
+// let key2 = 'key4';
+//
+// console.log(obj[key1][key2]);
+
+// task 44
+
+// let obj = {
+//     key1: {
+//         key2: '12',
+//         key3: '13',
+//     },
+//     key2: {
+//         key4: '24',
+//         key5: '25',
+//     },
+// }
+//
+// let key1 = 'key2';
+//
+// console.log(obj[key1]['key4']);
+
+//task 45
+
+// let employees = [
+//     {
+//         name: 'name1',
+//         salary: 300,
+//         age: 28,
+//     },
+//     {
+//         name: 'name2',
+//         salary: 400,
+//         age: 29,
+//     },
+//     {
+//         name: 'name3',
+//         salary: 500,
+//         age: 30,
+//     },
+// ];
+//
+// employees.push({
+//     name: 'name4',
+//     salary: 300,
+//     age: 27,
+// })
+//
+// console.log(employees);
+
+// task 46
+
+// let affairs = {
+//     '2019-12-28': ['data11', 'data12', 'data13'],
+//     '2019-12-29': ['data21', 'data22', 'data23'],
+//     '2019-12-30': ['data31', 'data32', 'data33'],
+// };
+//
+// affairs['2019-12-29'].push('data24');
+//
+// affairs['2019-12-31'] = [];
+//
+// affairs['2019-12-31'].push('data41', 'data42');
+//
+// console.log(affairs);
+
+// task 47
+
+// let students = {
+//     'group1': {
+//         'subgroup11': ['student111', 'student112', 'student113'],
+//         'subgroup12': ['student121', 'student122', 'student123'],
+//     },
+//     'group2': {
+//         'subgroup21': ['student211', 'student212', 'student213'],
+//         'subgroup22': ['student221', 'student222', 'student223'],
+//     },
+//     'group3': {
+//         'subgroup31': ['student311', 'student312', 'student313'],
+//         'subgroup32': ['student321', 'student322', 'student323'],
+//     },
+// };
+//
+// students.group1.subgroup11.push('student114');
+//
+// students.group1.subgroup13 = [];
+//
+// students.group4 = {};
+//
+// students.group4.subgroup41 = [];
+//
+// students.group4.subgroup41.push('student411', 'student412');
+//
+// console.log(students);
